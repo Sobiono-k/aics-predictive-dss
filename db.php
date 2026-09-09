4e7fcec4c644018a7531b4a48d718f7f22331114
KIEL SOBIONO <kianfrost24@gmail.com>
2026-09-09 10:47:02 +0800

    Fixed DB

--- db.php
+++ db.php
@@ -28,4 +28,4 @@ if ($host !== 'localhost' && $host !== '127.0.0.1') {
 if (!@mysqli_real_connect($conn, $host, $user, $pass, $DB_NAME, (int)$port, NULL, $flags)) {
     die("Database Connection Failed: " . mysqli_connect_error());
 }
-?>
+?> 
